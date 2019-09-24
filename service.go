6b65a98e932180d541159bf3b2dc1ecacaec7468
KoyamaSohei <koyamaso0309@gmail.com>
2019-09-25 02:37:05 +0900

    fix sql query

--- service.go
+++ service.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"database/sql"
+	"fmt"
 	"log"
 	"os"
 	"time"
@@ -20,7 +21,7 @@ var (
 
 func getDomainId(tx *sql.Tx, ctx context.Context, name string, account string) (string, error) {
 	var id string
-	err := tx.QueryRowContext(ctx, "SELECT id FROM domains WHERE name = '$1' AND account = '$2';", name, account).Scan(&id)
+	err := tx.QueryRowContext(ctx, "SELECT id FROM domains WHERE name = $1 AND account = $2;", name, account).Scan(&id)
 	if err != nil {
 		log.Fatal(err)
 		return "", err
@@ -33,7 +34,7 @@ func (s *server) InitZone(ctx context.Context, in *pb.InitZoneRequest) (*pb.Init
 	if err != nil {
 		return &pb.InitZoneResponse{Status: pb.ResponseStatus_InternalServerError}, err
 	}
-	_, err = tx.ExecContext(ctx, "INSERT INTO domains(name,type,account) VALUES ('$1','master','$2');", in.GetDomain(), in.GetAccount())
+	_, err = tx.ExecContext(ctx, "INSERT INTO domains(name,type,account) VALUES ($1,'master',$2);", in.GetDomain(), in.GetAccount())
 	if err != nil {
 		tx.Rollback()
 		return &pb.InitZoneResponse{Status: pb.ResponseStatus_InternalServerError}, err
@@ -43,7 +44,7 @@ func (s *server) InitZone(ctx context.Context, in *pb.InitZoneRequest) (*pb.Init
 		tx.Rollback()
 		return &pb.InitZoneResponse{Status: pb.ResponseStatus_InternalServerError}, err
 	}
-	_, err = tx.ExecContext(ctx, "INSERT INTO records(domain_id,name,type,content,change_date) VALUES ($1,'$2','SOA','$3 $4 $5 60 60 60 60',$5);", id, in.GetDomain(), mname, rname, time.Now().Unix()/1000)
+	_, err = tx.ExecContext(ctx, "INSERT INTO records(domain_id,name,type,content,change_date) VALUES ($1,$2,'SOA',$3,$4);", id, in.GetDomain(), fmt.Sprintf("%s %s %d 60 60 60 60", mname, rname, time.Now().Unix()/1000), time.Now().Unix()/1000)
 	if err != nil {
 		tx.Rollback()
 		return &pb.InitZoneResponse{Status: pb.ResponseStatus_InternalServerError}, err
